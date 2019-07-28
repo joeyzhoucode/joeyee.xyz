676ae63ecaf13d8655b80e2173401abe58cbf3be
Yi Zhou <yzhou.code@gmail.com>
2019-07-27 23:39:54 -0400

    Removing the need for :user_id for every message payload

--- app/channels/messenger_channel.rb
+++ app/channels/messenger_channel.rb
@@ -10,8 +10,8 @@ class MessengerChannel < ApplicationCable::Channel
   end
 
   def broadcast(data)
+    user = current_user
     group_name = data['group_name']
-    user = get_user(data['user_id'])
     content = data['content']
 
     raise 'No group_name!' if group_name.blank?
@@ -29,10 +29,6 @@ class MessengerChannel < ApplicationCable::Channel
   end
 
   private
-
-  def get_user(id)
-    User.find_by(id: id)
-  end
 
   def get_group(name)
     Group.find_by(name: name)
