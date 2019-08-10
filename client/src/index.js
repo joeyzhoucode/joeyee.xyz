045fa639d7955d6876f38afe71e681bcc281a39f
Yi Zhou <yzhou.code@gmail.com>
2019-08-09 20:26:53 -0400

    Registering service workers

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
+import * as serviceWorker from './serviceWorker';
 
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -20,4 +21,4 @@ const store = createStore(
 );
 
 render(<Root store={store} history={hist} />, document.getElementById("root"));
-
+serviceWorker.register();
